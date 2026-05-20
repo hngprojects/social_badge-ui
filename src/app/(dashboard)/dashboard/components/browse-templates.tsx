e9edff3bec06a6dddd177e5e48c33766becb57c7
kingsley <kingsleykay26@gmail.com>
2026-05-20 17:25:30 +0100

    fix: chnage svg properties to match tsx

--- src/app/(dashboard)/dashboard/components/browse-templates.tsx
+++ src/app/(dashboard)/dashboard/components/browse-templates.tsx
@@ -82,8 +82,8 @@ export default function BrowseTemplate() {
                         <path
                           d="M2.5 6H9.5"
                           stroke="#FF693E"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         />
                         <path
                           d="M6 2.5L9.5 6L6 9.5"
