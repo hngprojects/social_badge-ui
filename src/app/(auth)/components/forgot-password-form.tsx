c878669d7b26c04a926950f72657a4e035d63a44
Chidera paul ogbu <ogbupaul545@gmail.com>
2026-05-21 18:39:43 +0100

    fix: implement qa fixes for login signup and forgot password page

--- src/app/(auth)/components/forgot-password-form.tsx
+++ src/app/(auth)/components/forgot-password-form.tsx
@@ -9,79 +9,84 @@ import { useForm, useWatch } from "react-hook-form";
 import { forgotPasswordSchema, ForgotPasswordFormValues } from "@/schemas/auth";
 import Link from "next/link";
 export const ForgotPasswordForm = () => {
-    const [showModal, setShowModal] = useState(false);
-    const { forgotPassword, isLoading } = useForgotPassword();
+	const [showModal, setShowModal] = useState(false);
+	const { forgotPassword, isLoading } = useForgotPassword();
 
-    const {
-        register,
-        control,
-        handleSubmit,
-        formState: { errors, isSubmitting },
-    } = useForm<ForgotPasswordFormValues>({
-        resolver: zodResolver(forgotPasswordSchema),
-    });
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<ForgotPasswordFormValues>({
+		resolver: zodResolver(forgotPasswordSchema),
+	});
 
-    const email = useWatch({ control, name: "email" });
+	const email = useWatch({ control, name: "email" });
 
-    const onSubmit = (data: ForgotPasswordFormValues) => {
-        forgotPassword(
-            { email: data.email },
-            { onSuccess: () => setShowModal(true) },
-        );
-    };
+	const onSubmit = (data: ForgotPasswordFormValues) => {
+		forgotPassword(
+			{ email: data.email },
+			{ onSuccess: () => setShowModal(true) },
+		);
+	};
 
-    return (
-        <>
-            <div className="login-form bg-[#F5F5F5] rounded-lg px-5 py-6 flex flex-col gap-7">
-                <form className="flex flex-col gap-7" onSubmit={handleSubmit(onSubmit)} noValidate>
-                    <div className="flex flex-col gap-4">
-                        <AuthInput
-                            disabled={isSubmitting || isLoading}
-                            type="email"
-                            placeholder="usersocialbadge@hng.com"
-                            label={"Email"}
-                            {...register("email")}
-                        />
-                        {errors.email && (
-                            <p className="text-[#EF4444] text-xs mt-1">
-                                {errors.email.message}
-                            </p>
-                        )}
-                    </div>
+	return (
+		<>
+			<div className="login-form bg-[#F5F5F5] rounded-lg px-5 py-6 flex flex-col gap-7">
+				<form
+					className="flex flex-col gap-7"
+					onSubmit={handleSubmit(onSubmit)}
+					noValidate
+				>
+					<div className="flex flex-col gap-4">
+						<AuthInput
+							disabled={isSubmitting || isLoading}
+							type="email"
+							placeholder="usersocialbadge@hng.com"
+							label={"Email"}
+							{...register("email")}
+						/>
+						{errors.email && (
+							<p className="text-[#EF4444] text-xs mt-1">
+								{errors.email.message}
+							</p>
+						)}
+					</div>
 
-                    <Button disabled={isSubmitting || isLoading} type="submit">
-                        {isLoading ? "Sending reset link..." : "Send Reset Link"}
-                    </Button>
-                </form>
+					<Button disabled={isSubmitting || isLoading} type="submit">
+						{isLoading ? "Resetting Password..." : "Reset Password"}
+					</Button>
+				</form>
 
-                <div className="text-center">
-                    <p className="text-md">
-                        Go back to{" "}
+				<div className="text-center">
+					<p className="text-md">
+						Go back to{" "}
+						<Link
+							href="/login"
+							className="font-bold text-[#FA5424] hover:text-[#e14b1c]"
+						>
+							Log in
+						</Link>
+					</p>
+				</div>
+			</div>
 
-                        <Link
-                            href="/login"
-                            className="font-bold text-[#FA5424] hover:text-[#e14b1c]"
-                        >
-                            Log in
-                        </Link>
-                    </p>
-                </div>
-            </div>
-
-            {showModal && (
-                <AuthModal
-                    closeModal={() => setShowModal(false)}
-                    email={email}
-                    imageSrc="/assets/auth-flow/mail-img.png"
-                    title="We've sent you a reset link"
-                    description={
-                        <>
-                            <p>We&apos;ve sent instructions to your registered email address.</p>
-                            <p>Click the link in the email to reset your password.</p>
-                        </>
-                    }
-                />
-            )}
-        </>
-    );
+			{showModal && (
+				<AuthModal
+					closeModal={() => setShowModal(false)}
+					email={email}
+					imageSrc="/assets/auth-flow/mail-img.png"
+					title="We've sent you a reset link"
+					description={
+						<>
+							<p>
+								We&apos;ve sent instructions to your registered email address.
+							</p>
+							<p>Click the link in the email to reset your password.</p>
+						</>
+					}
+				/>
+			)}
+		</>
+	);
 };
