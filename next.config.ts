3baa60672814452bcbb0be7e3d9148d17e9e8c5e
odielijah <odi.esther.elijah@gmail.com>
2026-05-23 13:22:14 +0100

    undo changes in the next config

--- next.config.ts
+++ next.config.ts
@@ -12,14 +12,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.staging.social-badge.hng14.com/api/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
