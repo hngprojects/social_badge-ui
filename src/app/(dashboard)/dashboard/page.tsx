ee31dbcc10d4967477d2755fb8879dd5a71829d9
kingsley <kingsleykay26@gmail.com>
2026-05-25 08:46:15 +0100

    refactor:change  default welcome banner on dashboard to a component

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useUserStore } from "@/stores/use-user-store";
+import { UserWelcome } from "../components/dashboard/user-welcome";
 import Analytics from "../components/dashboard/analytics";
 import BrowseTemplate from "../components/dashboard/browse-templates";
 import { FirstBadgeCta } from "../components/dashboard/first-badge-cta";
@@ -10,34 +10,20 @@ import Steps from "../components/dashboard/steps";
 import { useRecentOrganizerBadges } from "../hooks/use-organizer-template-instances";
 
 export default function Dashboard() {
-  const user = useUserStore((state) => state.user);
-  const userName = user?.first_name ?? "there";
-
   const { templates } = useRecentOrganizerBadges(RECENT_BADGES_LIMIT);
 
+  // count of badge numbe. This should be wired to get total badge count
+  // const hasBadges = templates.length > 0;
   const hasBadges = templates.length > 0;
 
   return (
     <section className="flex flex-col gap-6 pt-[32px]">
       <header>
         <h1 className="capitalize text-[#AFAFAF] text-[14px]">Dashboard</h1>
-
-        <div className="my-[8px]">
-          <p className="text-[28px] font-bold text-[#1A1A1A]">
-            Welcome to Social Badge,{" "}
-            <span className="italic font-fraunces text-[#FF693E]">
-              {userName}
-            </span>
-          </p>
-
-          <p className="text-[14px] text-[#9B9B9B]">
-            Let&apos;s design your first badge - your attendees will be sharing
-            it before the day&apos;s out.
-          </p>
-        </div>
+        <UserWelcome />
       </header>
 
-      <Analytics />
+      <Analytics templates={templates} />
 
       {hasBadges ? <RecentBadges /> : <FirstBadgeCta />}
 
