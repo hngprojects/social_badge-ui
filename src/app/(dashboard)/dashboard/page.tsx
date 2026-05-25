4c654aa03c0cd0886a002d73fe3b8cd8e5b21421
kingsley <kingsleykay26@gmail.com>
2026-05-25 17:13:22 +0100

    feat: conditionally render dashboard CTA and recent badges

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,21 @@
 "use client";
-import BrowseTemplate from "../components/dashboard/browse-templates";
-import Steps from "../components/dashboard/steps";
+
 import { UserWelcome } from "../components/dashboard/user-welcome";
+import Analytics from "../components/dashboard/analytics";
+import BrowseTemplate from "../components/dashboard/browse-templates";
 import { FirstBadgeCta } from "../components/dashboard/first-badge-cta";
+import RecentBadges from "../components/dashboard/recent-badges";
+import { RECENT_BADGES_LIMIT } from "../components/dashboard/recent-badges-types";
+import Steps from "../components/dashboard/steps";
+import { useRecentOrganizerBadges } from "../hooks/use-organizer-template-instances";
 
 export default function Dashboard() {
+  const { templates } = useRecentOrganizerBadges(RECENT_BADGES_LIMIT);
+
+  // count of badge numbe. This should be wired to get total badge count
+  // const hasBadges = templates.length > 0;
+  const hasBadges = templates.length > 0;
+
   return (
     <section className="flex flex-col gap-6 pt-[32px]">
       <header>
@@ -12,9 +23,12 @@ export default function Dashboard() {
         <UserWelcome />
       </header>
 
-      <FirstBadgeCta />
+      <Analytics templates={templates} />
+
+      {hasBadges ? <RecentBadges /> : <FirstBadgeCta />}
 
       <Steps />
+
       <BrowseTemplate />
     </section>
   );
