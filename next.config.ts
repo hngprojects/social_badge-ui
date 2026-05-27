c2ddcb8f925c64e2d8ba198622e44ea9318d823b
kingsley <kingsleykay26@gmail.com>
2026-05-27 19:35:26 +0100

    chore: add assets for settings page

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,26 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     authInterrupts: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "api.qrserver.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -16,6 +39,14 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.staging.social-badge.hng14.com/api/:path*",
+      },
+    ];
   },
 };
 
