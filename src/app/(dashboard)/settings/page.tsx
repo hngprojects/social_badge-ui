45be228e82db82096d650045883ec776ca622fef
kingsley <kingsleykay26@gmail.com>
2026-05-28 09:26:23 +0100

    feat(dashboard): scaffold settings page structure

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,3 +1,11 @@
+import SettingTabs from "./components/settings-pages";
+import SettingsHeader from "./components/settings-header";
+
 export default function Settings() {
-  return <>Settings</>;
+  return (
+    <section className="flex flex-col gap-6 pt-8 h-screen max-w-[768px]">
+      <SettingsHeader />
+      <SettingTabs />
+    </section>
+  );
 }
