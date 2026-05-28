ec46e84ad3a4e1f771cccab5b4376fc2b866391a
kingsley <kingsleykay26@gmail.com>
2026-05-28 12:09:24 +0100

    chore(dashboard): normalize tailwind utility classes

--- src/app/(dashboard)/settings/components/input-profile.tsx
+++ src/app/(dashboard)/settings/components/input-profile.tsx
@@ -11,34 +11,34 @@ import { Asterisk } from "lucide-react";
 export function ProfileInput() {
   return (
     <FieldSet className="w-full text-[14px] font-normal text-[#B5B7BC]">
-      <FieldGroup className="gap-[16px]">
+      <FieldGroup className="gap-4">
         {/* FULL NAME */}
-        <Field className="w-full gap-[8px]">
+        <Field className="w-full gap-2">
           <FieldLabel
             className="w-full text-[#3A3A3A] gap-0 font-medium"
             htmlFor="fullname"
           >
             Full name
-            <Asterisk className="w-[8px] h-auto text-error-500 font-medium tracking-normal leading-0" />
+            <Asterisk className="w-2 h-auto text-error-500 font-medium tracking-normal leading-0" />
           </FieldLabel>
           <Input
-            className="w-full rounded-[8px] py-[8px] px-[16px] text-[14px] text-[#121217] placeholder:text-[#B5B7BC] font-medium bg-[#E7E8E94D]/30"
+            className="w-full rounded-[8px] py-2 px-4 text-[14px] text-[#121217] placeholder:text-[#B5B7BC] font-medium bg-[#E7E8E94D]/30"
             id="fullname"
             type="text"
             placeholder="Andrew Smith"
           />
         </Field>
         {/* EMAIL ADDRESS*/}
-        <Field className="w-full gap-[8px]">
+        <Field className="w-full gap-2">
           <FieldLabel
             className="w-full text-[#3A3A3A] gap-0 font-medium"
             htmlFor="email"
           >
             Email address
-            <Asterisk className="w-[8px] h-auto text-error-500" />
+            <Asterisk className="w-2 h-auto text-error-500" />
           </FieldLabel>
           <Input
-            className="w-full rounded-[8px] py-[8px] px-[16px] text-[14px] text-[#121217] placeholder:text-[#B5B7BC] bg-[#E7E8E94D]/30"
+            className="w-full rounded-[8px] py-2 px-4 text-[14px] text-[#121217] placeholder:text-[#B5B7BC] bg-[#E7E8E94D]/30"
             id="fullname"
             type="text"
             placeholder="Andresmith@achieverher.com"
@@ -48,7 +48,7 @@ export function ProfileInput() {
           </FieldDescription>
         </Field>
         {/* ROLE/TITLE*/}
-        <Field className="w-full gap-[8px]">
+        <Field className="w-full gap-2">
           <FieldLabel
             className="w-full text-[#121217] gap-0 font-medium"
             htmlFor="email"
@@ -56,7 +56,7 @@ export function ProfileInput() {
             Role / Title
           </FieldLabel>
           <Input
-            className="w-full rounded-[8px] py-[8px] px-[16px] font-medium text-[14px] text-[#121217] bg-[#E7E8E94D]/30"
+            className="w-full rounded-[8px] py-2 px-4 font-medium text-[14px] text-[#121217] bg-[#E7E8E94D]/30"
             id="fullname"
             type="text"
           />
