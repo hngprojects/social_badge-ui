a7346b4c4c34e12c4b36790312014770f8548cc0
kingsley <kingsleykay26@gmail.com>
2026-05-29 21:57:24 +0100

    feat: update analytis copy

--- src/app/(dashboard)/constants/dashboard.ts
+++ src/app/(dashboard)/constants/dashboard.ts
@@ -68,7 +68,7 @@ export const stats: StatCard[] = [
   },
   {
     image: "/assets/dashboard/icons/ui-link.svg",
-    title: "total link clicked",
+    title: "total link clicks",
     count: "0",
     bg: "#DCE6FD",
   },
