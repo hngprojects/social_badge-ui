d2822989894eed804ce9729b7cfcfd260ccdc288
kingsley <kingsleykay26@gmail.com>
2026-06-27 21:26:38 +0100

    refactor(watermark): resize logo and center watermark

--- src/app/features/templates/components/badge-preview/watermark.tsx
+++ src/app/features/templates/components/badge-preview/watermark.tsx
@@ -3,11 +3,11 @@ import { WatermarkProps } from "@/app/features/templates/components/badge-previe
 
 export function Watermark({watermarkColor, watermarkLogo, watermarkBrandName}: WatermarkProps){
   return (
-    <div className="text-[8px] font-light flex justify-end" style={{color: watermarkColor}}>
+    <div className="text-[8px] font-light flex justify-center mt-1" style={{color: watermarkColor}}>
           <div className="flex gap-1">
-  {watermarkLogo && <Image className="opacity-70" src={watermarkLogo} alt="Flare tag's logo" width={9} height={9} />
+  {watermarkLogo && <Image className="opacity-70" src={watermarkLogo} alt="Flare tag's logo" width={7} height={7} />
   }
-          <span className="opacity-45">{watermarkBrandName}</span>
+          <span className="opacity-40">{watermarkBrandName}</span>
           
           </div>
   {/* Pass URL only when approved by PO, and URL is stable */}
